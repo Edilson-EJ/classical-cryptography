f39abeece88074921e6f5eb64d54b7534a6de637
Edilson-EJ <edilson.franca.melo@gmail.com>
2024-10-14 15:02:59 -0300

    formulation of the logic of transposition cryptography.

--- src/app/Pages/caesar-cipher-algorithm/caesar-cipher-algorithm.component.ts
+++ src/app/Pages/caesar-cipher-algorithm/caesar-cipher-algorithm.component.ts
@@ -8,11 +8,12 @@ import {
 } from '@angular/forms';
 import { CaesarCipherAlgorithmServiceService } from '../../Services/caesar-cipher-algorithm-service.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-caesar-cipher-algorithm',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './caesar-cipher-algorithm.component.html',
   styleUrls: ['./caesar-cipher-algorithm.component.scss'],
 })
@@ -21,14 +22,12 @@ export class CaesarCipherAlgorithmComponent {
   private formBuilder = inject(NonNullableFormBuilder);
 
   resultText: string = '';
-
-  // Definir o formulário reativo
+  copiedMessage: string | null = null;
   cipherForm: FormGroup = this.formBuilder.group({
     inputText: ['', Validators.required],
     shift: [3, [Validators.required, Validators.min(1)]],
   });
 
-  // Função para criptografar o texto
   encrypt(): void {
     const { inputText, shift } = this.cipherForm.value;
     if (this.cipherForm.valid) {
@@ -36,11 +35,34 @@ export class CaesarCipherAlgorithmComponent {
     }
   }
 
-  // Função para descriptografar o texto
   decrypt(): void {
     const { inputText, shift } = this.cipherForm.value;
     if (this.cipherForm.valid) {
       this.resultText = this.cipherService.decrypt(inputText, shift);
     }
+  }
+
+  copyToClipboard() {
+    if (this.resultText) {
+      navigator.clipboard
+        .writeText(this.resultText)
+        .then(() => {
+          this.copiedMessage = 'Texto copiado para a área de transferência!';
+          setTimeout(() => {
+            this.copiedMessage = null;
+          }, 3000);
+        })
+        .catch((err) => {
+          console.error('Erro ao copiar o texto: ', err);
+        });
+    } else {
+      console.log('Não há texto para copiar.');
+    }
+  }
+
+  clearInput() {
+    this.cipherForm.reset();
+    this.resultText = '';
+    this.copiedMessage = null;
   }
 }
--- src/app/Pages/transposition-ciphers/transposition-ciphers.component.ts
+++ src/app/Pages/transposition-ciphers/transposition-ciphers.component.ts
@@ -1,54 +1,84 @@
 import { Component, inject } from '@angular/core';
 import {
+  FormControl,
+  FormGroup,
   NonNullableFormBuilder,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { TranspositionCiphersServiceService } from '../../Services/transposition-ciphers-service.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transposition-ciphers',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, CommonModule],
   templateUrl: './transposition-ciphers.component.html',
   styleUrls: ['./transposition-ciphers.component.scss'],
 })
 export class TranspositionCiphersComponent {
-  private cipherService = inject(TranspositionCiphersServiceService);
-  private formBuild$ = inject(NonNullableFormBuilder);
-
-  // Formulário reativo
-  transpositionCiphersForm = this.formBuild$.group({
-    inputText: ['', [Validators.required]],
-    numColumns: [4, [Validators.required, Validators.min(1)]], // Número de colunas padrão
-  });
+  private formBuilder = inject(NonNullableFormBuilder);
+  private transpositionService = inject(TranspositionCiphersServiceService); // Injeção do serviço
 
   resultText: string = '';
+  copiedMessage: string | null = null;
 
-  constructor() {}
+  transpositionCiphersForm: FormGroup = this.formBuilder.group({
+    inputText: ['', Validators.required],
+    key: ['', [Validators.required, this.uniqueLettersValidator]],
+  });
 
-  // Função para criptografar o texto
+  // Validador para garantir que a chave não contenha letras repetidas
+  uniqueLettersValidator(control: FormControl) {
+    const value = control.value;
+    const uniqueLetters = new Set(value.split(''));
+    return uniqueLetters.size === value.length
+      ? null
+      : { duplicateLetters: true };
+  }
+
   encrypt(): void {
-    const { inputText, numColumns } = this.transpositionCiphersForm.value;
+    const { inputText, key } = this.transpositionCiphersForm.value;
 
-    // Verifique se numColumns é um número válido
-    if (typeof numColumns === 'number' && typeof inputText === 'string') {
-      this.resultText = this.cipherService.encrypt(inputText, numColumns);
+    // Utiliza o tamanho da chave como o número de colunas
+    const numColumns = key.length;
+
+    // Utilizando o serviço para cifrar
+    this.resultText = this.transpositionService.encrypt(inputText, numColumns);
+  }
+
+  decrypt(): void {
+    const { inputText, key } = this.transpositionCiphersForm.value;
+
+    // Utiliza o tamanho da chave como o número de colunas
+    const numColumns = key.length;
+
+    // Utilizando o serviço para descriptografar
+    this.resultText = this.transpositionService.decrypt(inputText, numColumns);
+  }
+
+  copyToClipboard() {
+    if (this.resultText) {
+      navigator.clipboard
+        .writeText(this.resultText)
+        .then(() => {
+          this.copiedMessage = 'Texto copiado para a área de transferência!';
+          setTimeout(() => {
+            this.copiedMessage = null;
+          }, 3000);
+        })
+        .catch((err) => {
+          console.error('Erro ao copiar o texto: ', err);
+        });
     } else {
-      this.resultText = 'Texto ou número de colunas inválido.';
+      console.log('Não há texto para copiar.');
     }
   }
 
-  // Função para descriptografar o texto
-  decrypt(): void {
-    const { inputText, numColumns } = this.transpositionCiphersForm.value;
-
-    // Verifique se numColumns é um número válido
-    if (typeof numColumns === 'number' && typeof inputText === 'string') {
-      this.resultText = this.cipherService.decrypt(inputText, numColumns);
-    } else {
-      this.resultText = 'Texto ou número de colunas inválido.';
-    }
+  clearInput() {
+    this.transpositionCiphersForm.reset();
+    this.resultText = '';
+    this.copiedMessage = null;
   }
 }
